c6ab24dc48e5e9ebb9daf364baf2f759f358967f
GenaGrig <gennadiuss@gmail.com>
2023-07-06 20:14:04 +0000

    Deployment commit

--- genstar/settings.py
+++ genstar/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR=os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2_+pn(o&21!#91y58ar5s2-1e)t5$_*k4+a32i%sv+3&5j+f='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'fitness',
 ]
 
@@ -55,7 +64,7 @@ ROOT_URLCONF = 'genstar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['TEMPLATES_DIR'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,12 +83,16 @@ WSGI_APPLICATION = 'genstar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={ 
+        'default': dj_database_url.parse(os.environ.get("DATABASE_ URL"))
+        }
 
 
 # Password validation
@@ -117,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE= 'cloudinary_storage.storage.StaticHashedCloudin aryStorage'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL='/media/'
+DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStor age'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

8aff621a4c0a56dec168b18d0b72535a8bb36b45
GenaGrig <gennadiuss@gmail.com>
2023-07-09 21:14:32 +0000

    Add: templates base- and index.html and its work test

--- fitness/views.py
+++ fitness/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Genstar Fitness site.")
+class MainPageView(generic.TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Genstar'
+        return context
+    
+    
+
--- genstar/settings.py
+++ genstar/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'fitness.apps.FitnessConfig',
+    # 'fitness.apps.FitnessConfig',
     'cloudinary',
+    'fitness',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'genstar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES_DIR'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

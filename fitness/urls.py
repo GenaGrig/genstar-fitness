70a4d00421324fdac573625c14a576dd5fdc0573
GenaGrig <gennadiuss@gmail.com>
2023-07-09 22:04:38 +0000

    Fix: adjusting the nav bar, adding links to it

--- fitness/urls.py
+++ fitness/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='index'),
+    # path('login/', views.LoginView.as_view(), name='login'),
+    # path('logout/', views.logout_view, name='logout'),
+    # path('register/', views.RegisterView.as_view(), name='register'),
+    # path('profile/', views.ProfileView.as_view(), name='profile'),
 ]

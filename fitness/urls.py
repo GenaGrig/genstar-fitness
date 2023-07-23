61c0f112971c043d88a09756b3d9369dd56dff5d
GenaGrig <gennadiuss@gmail.com>
2023-07-23 17:54:33 +0000

    Add: personal trainer page with content and styling

--- fitness/urls.py
+++ fitness/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.MainPageView.as_view(), name='index'),
     path('membership/', views.MembershipView.as_view(), name='membership'),
     path('workouts/', views.WorkoutsView.as_view(), name='workouts'),
+    path('personal_trainer/', views.PersonalTrainerView.as_view(), name='personal_trainer'),
     # path('login/', views.LoginView.as_view(), name='login'),
     # path('logout/', views.logout_view, name='logout'),
     # path('register/', views.RegisterView.as_view(), name='register'),
--- fitness/views.py
+++ fitness/views.py
@@ -27,3 +27,12 @@ class WorkoutsView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Workouts'
         return context
+
+
+class PersonalTrainerView(generic.TemplateView):
+    template_name = 'personal_trainer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'personal_trainer'
+        return context

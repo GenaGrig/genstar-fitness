e9bb341e40a721fd4f518ab9a3844263278ec8ac
GenaGrig <gennadiuss@gmail.com>
2023-08-30 16:55:19 +0000

    Fix error with media module to correct deployment

--- genstar/settings.py
+++ genstar/settings.py
@@ -180,6 +180,9 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
